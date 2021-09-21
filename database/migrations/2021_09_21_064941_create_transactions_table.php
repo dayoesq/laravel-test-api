bb044b3ab04cb7d88fa726d50615f3253719abb4
Dayo <dayoesq@live.com>
2021-09-21 15:01:22 +0300

    Added tables and migrations

--- database/migrations/2021_09_21_064941_create_transactions_table.php
+++ database/migrations/2021_09_21_064941_create_transactions_table.php
@@ -15,7 +15,13 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->integer('quantity')->unsigned();
+            $table->integer('buyer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('buyer').references('id')->on('users');
+            $table->foreign('product').references('id')->on('products');
         });
     }
 
